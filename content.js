2ecea424c91d8116e39148bce2be293e0a533ca0
Ron Shaked <ronshaked07@gmail.com>
2023-10-08 18:25:27 -0500

    Working on Weighted

--- content.js
+++ content.js
@@ -46,19 +46,65 @@ function calculateUnweighted() {
   return unweightedRaw.toFixed(2);
 }
 
+const classes = findClasses();
+const formattedDateTime = formatDateAndTime();
+const unweightedGPA = calculateUnweighted();
+const weightedGPA = calculateWeighted();
+
+console.log(classes);
+
+let levelIIExceptions = new Set();
+levelIIExceptions.add("Calculus");
+levelIIExceptions.add("Discrete Mathematics for Computer Science");
+levelIIExceptions.add("Computer Science I");
+
 function calculateWeighted() {
+
+
+  let sumOfWeights = 0;
+
+  for (let i = 0; i < classes.length; i++) {
+    const currWeight = findWeight(classes[i]);
+    console.log(`${classes[i]} ${currWeight}`);
+    sumOfWeights += currWeight;
+    console.log(sumOfWeights);
+  }
+
+  console.log(sumOfWeights);
+
   const [GPADifference, numOfClasses] = findGrades();
   let weightedRaw = (numOfClasses * 4  - GPADifference) / numOfClasses;
 
   return weightedRaw.toFixed(2);
 }
 
-const formattedDateTime = formatDateAndTime();
-const unweightedGPA = calculateUnweighted();
-const weightedGPA = calculateWeighted();
-const classes = findClasses();
 
-console.log(classes);
+function findWeight(className) {
+  const classNameArray = className.split(" ");
+
+  if (levelIIExceptions.has(className)) {
+    return 4.5;
+  }
+
+  for (let i = 0; i < classNameArray.length; i++) {
+    if (classNameArray[i] === "AP" || classNameArray[i] === "IB" || classNameArray[i] === "Practicum") {
+      return 5.0;
+    }
+
+    if (classNameArray[i] === "DC" || classNameArray[i] === "Advanced" || classNameArray[i] === "Dual") {
+     return 4.5
+     
+    }
+
+
+  }
+  return 4.0;
+}
+
+
+
+
+
 
 
 const injectedHTML = `
