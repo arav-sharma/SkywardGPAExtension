ab5f411354d2706bc0424c5cb7ebdd74030fe775
Arav Sharma <142256252+arav-sharma@users.noreply.github.com>
2023-10-09 19:07:53 -0500

    Added notes and rearranged  code for readability

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// Creates a set of courses that do not have "advanced" or "DC/Dual Credit" in the class name that are weighted as 4.50
 let levelIIExceptions = new Set([
   "Calculus",
   "Discrete Mathematics for Computer Science",
@@ -9,9 +10,12 @@ let levelIIExceptions = new Set([
   "Anatomy & Physiology",
   "Pharmacy I",
   "Instructional Practices",
-  "Independent Study Journalism I, II",
-  "Advanced Journalism: Yearbook II, III (Editors)",
-  "Advanced Journalism: Newspaper II, III (Editors)",
+  "Independent Study Journalism I",
+  "Independent Study Journalism II",
+  "Advanced Journalism Yearbook II",
+  "Advanced Journalism Yearbook III",
+  "Advanced Journalism Newspaper II",
+  "Advanced Journalism Newspaper III",
   "Business Computer Applications",
   "Networking/Networking Lab",
   "Evolving/Emerging Technologies-Esports II",
@@ -33,6 +37,8 @@ let levelIIExceptions = new Set([
   "Advanced Music Choir 10",
   "Advanced Music Band 10"
 ]);
+
+// Creates a set that includes courses that do not have "AP" or "IB" in the class name but are weighted as 5.0
 const levelIIIExceptions = new Set([
   "Computer Science III",
   "Robotics II",
@@ -46,14 +52,14 @@ const levelIIIExceptions = new Set([
   "Advanced Music Band 12"
 ]);
 
-
-
+// Finds the user's exact date, time and timezone
 function formatDateAndTime() {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
     const formattedDateTime = new Intl.DateTimeFormat('en-US', options).format(new Date());
     return formattedDateTime;
 }
 
+// Find the user's SM1 Grades
 function findGrades () {
   let gradesGPA = [];
   let rawGPA = 0;
@@ -76,11 +82,12 @@ function findGrades () {
   return [rawGPA, gradesGPA.length];
 }
 
+// Finds the user's classes
 function findClasses() {
   let classes = []; 
   const classNames = document.querySelectorAll('table tbody tr');
 
-  classNames.forEach(row => {
+  classNames.forEach((row) => {
     const courseNameElement = row.querySelector('.classDesc a[href="javascript:void(0)"]');
     if (courseNameElement) {
       const courseName = courseNameElement.textContent.trim();
@@ -93,6 +100,7 @@ function findClasses() {
   return classes;
 }
 
+// Calculates the unweighted GPA of the student
 function calculateUnweighted() {
   const [GPADifference, numOfClasses] = findGrades();
   let unweightedRaw = (numOfClasses * 4 - GPADifference) / numOfClasses;
@@ -100,29 +108,7 @@ function calculateUnweighted() {
   return unweightedRaw.toFixed(2);
 }
 
-
-
-const classes = findClasses();
-const formattedDateTime = formatDateAndTime();
-const unweightedGPA = calculateUnweighted();
-const weightedGPA = calculateWeighted();
-
-
-function calculateWeighted() {
-  let sumOfWeights = 0;
-
-  for (let i = 0; i < classes.length; i++) {
-    const currWeight = findWeight(classes[i]);
-    sumOfWeights += currWeight;
-  }
-
-  const [GPADifference, numOfClasses] = findGrades();
-  let weightedRaw = (sumOfWeights - GPADifference) / numOfClasses;
-
-  return weightedRaw.toFixed(2);
-}
-
-
+// Finds the weight of a specific class
 function findWeight(className) {
   const classNameArray = className.split(" ");
 
@@ -143,16 +129,37 @@ function findWeight(className) {
      return 4.5
      
     }
-
-
   }
   return 4.0;
 }
 
+// Invokes functions and finds GPAs
+const classes = findClasses();
+const formattedDateTime = formatDateAndTime();
+const unweightedGPA = calculateUnweighted();
+const weightedGPA = calculateWeighted();
+
+// Calculates the weighted GPA of the student
+function calculateWeighted() {
+  let sumOfWeights = 0;
+
+  for (let i = 0; i < classes.length; i++) {
+    const currWeight = findWeight(classes[i]);
+    sumOfWeights += currWeight;
+  }
+
+  const [GPADifference, numOfClasses] = findGrades();
+  let weightedRaw = (sumOfWeights - GPADifference) / numOfClasses;
+
+  return weightedRaw.toFixed(2);
+}
+
+// HTML to be in added to the website
 const injectedHTML = `
     <div id="missingAssignmentsModuleWrapper"><div id="grid_missingAssignmentsModule_gridWrap" grid-theme="summary" class="gridWrap"><div class="sf_gridTableWrap"><table vpaginate="no" id="grid_missingAssignmentsModule" grid-table="summary" zebra="false"><tbody><tr class=""><td scope="row">Your weighted GPA is: ${weightedGPA} and your unweighted GPA is: ${unweightedGPA} as of ${formattedDateTime}.</td></tr></tbody></table></div></div></div>
 `;
 
+// Finds where to inject the HTML 
 function injectGPA() {
   const targetDiv = document.getElementById("missingAssignmentsModuleWrapper");
 
@@ -163,6 +170,7 @@ function injectGPA() {
   }
 }
 
+// Defnines targetURL and ensures that the user is on the correct URL before injected HTML (failsafe)
 const targetURL = "https://skyward.iscorp.com/scripts/wsisa.dll/WService=wseduallenisdtx/sfgradebook001.w";
 
 if (window.location.href.startsWith(targetURL)) {
